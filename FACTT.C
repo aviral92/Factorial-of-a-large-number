4a860dd5c238b3fb3e8bd8823b8dda8d1ba89acf
Aviral Khattar <aviral92@gmail.com>
2018-10-03 00:16:11 -0400

    Update FACTT.C

--- FACTT.C
+++ FACTT.C
@@ -152,16 +152,3 @@ void multiply(int n[],int f[],int a[])
 
 	return;
 }
-
-output:
-
-Enter the number : 33
-
-
-Factorial of the number is : 8683317618811886495518194401280000000
-
-
-Enter the number : 60
-
-
-Factorial of the number is :8320987112741390144276341183223364380754172606361245952449277696409600000000000000
